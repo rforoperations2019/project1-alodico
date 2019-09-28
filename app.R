a41db5f06c7296c64b7c43fb403e2a17b71ccfca
antoniolodico <antoniolodico@users.noreply.github.com>
2019-09-28 11:12:34 -0400

    Added two inputs, draft third input, and scatterplot output.

--- app.R
+++ app.R
@@ -9,63 +9,69 @@
 
 library(shiny)
 library(ggplot2)
+library(dplyr)
 #reading and merging data files for geography names with FIPS
 CCFIPS <- read.csv(file="citycountyfips.csv")
 SASMCD <- read.csv(file="SAS_County_Data.csv")
-CountyDataWNames <- merge(CCFIPS, 
-                          SASMCD, 
-                          by.x = "CityCountyFIPS", 
-                          by.y= "FIPS")
-
-#LAOPR80 scatterplot
-ggplot(CountyDataWNames, aes(x=Long_Acting_Opioid_Pres2, 
-                             y=Opioid_Prescribing_Rate2,
-                             color=target_LAOPR_80)) + geom_point()
-#LAOPR90 scatterplot
-ggplot(CountyDataWNames, aes(x=Long_Acting_Opioid_Pres2, 
-                             y=Opioid_Prescribing_Rate2,
-                             color=target_LAOPR_90)) + geom_point()
-
+CountyData <- merge(CCFIPS,
+                    SASMCD, 
+                    by.x = "CityCountyFIPS", 
+                    by.y= "FIPS")
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("County Level Opioid Data"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
+            #This is a range slider that allows you to choose which year(s) of data you are looking at
+            sliderInput("years",
+                        label = "Include data from year(s):",
+                        min = 2013,
+                        max = 2017,
+                        value = c(2013,2017)),
+            #and this select box lets you choose the dependent variable
+            selectInput("DepVar", label = h3("Choose Dependent Variable"), 
+                        choices = list("Opioid Prescription Rate" = "Opioid_Prescribing_Rate2", 
+                                       "Long Acting Opioid Prescription Rate" = "Long_Acting_Opioid_Pres2",
+                                       "Binomial Target Variable 80" = "target_LAOPR_80", 
+                                       "Binomial Target Variable 90" = "target_LAOPR_90")
+                        ),
+            #and this select box lets you choose the independent variable
+            selectInput("IndVar", label = h3("Choose Independent Variable"), 
+                        choices = list("Opioid Prescription Rate" = "Opioid_Prescribing_Rate2", 
+                                       "Long Acting Opioid Prescription Rate" = "Long_Acting_Opioid_Pres2",
+                                       "Binomial Target Variable 80" = "target_LAOPR_80", 
+                                       "Binomial Target Variable 90" = "target_LAOPR_90")
+                        )
+            ),
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot"),
             plotOutput("testplot")
         )
     )
 )
 
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
+    newdata <- reactive({CountyData %>% filter(year_num %in% (input$years[1]:input$years[2]))
+        })
+    # newy <- observe({
+    #     print(input$DepVar)
+    # })
+    # obsB <- observe({
+    #     print(input$years[1])
+    #     print(input$years[2])
+    # })
     output$testplot <- renderPlot({
-        ggplot(CountyDataWNames, aes(x=Long_Acting_Opioid_Pres2, 
-                                 y=Opioid_Prescribing_Rate2,
-                                 color=target_LAOPR_80)) + geom_point()
+        ggplot(newdata(), aes_string(x=input$IndVar,
+                                 y=input$DepVar,
+                                 color="target_LAOPR_80")) + geom_point()
     })
 }
 
